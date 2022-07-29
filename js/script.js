7a0ec4743a2cc6c43c6eaf336119ee61aaa5d0ba
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-07-29 19:31:47 +0300

    simple site ready. configure JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hamburgerMenu = document.querySelector('.nav__hamburger-btn');
+const hamburgerOpen = document.querySelector('.nav__hamburger-btn_open')
+const hamburgerClose = document.querySelector('.nav__hamburger-btn_close')
+
+
+hamburgerMenu.addEventListener("click", () => {
+
+})
