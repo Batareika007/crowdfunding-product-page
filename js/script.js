fa78be5a4a076602642fba03d807f9008281dd8c
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-09-07 22:56:30 +0300

    added bookmark event listener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 let hamburger = document.querySelector('.hamburger');
 let navList = document.querySelector('.nav-list');
+let bookmarkBtn = document.querySelector('.block__btn2');
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     navList.classList.toggle('is-active');
+})
+
+bookmarkBtn.addEventListener('click', () => {
+    bookmarkBtn.classList.add('active-btn');
+    document.querySelector('.button-text').innerHTML = "Bookmarked"
 })
