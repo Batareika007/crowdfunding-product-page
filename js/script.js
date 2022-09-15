d55dc249ccb1ba1c636b6b11f68bf00305d66ee1
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-09-15 17:10:48 +0300

    style modal block + some JS
    start to write scripts for modal

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ bookmarkBtn.addEventListener('click', () => {
     bookmarkBtn.classList.add('active-btn');
     document.querySelector('.button-text').innerHTML = "Bookmarked"
 })
+
+let modalNoReward = document.querySelector('.modal__no-rew');
+let modalBamboo = document.querySelector('.modal__bamboo');
+let modalBlack = document.querySelector('.modal__black');
+let modalMahogany = document.querySelector('.modal__mahogany');
+
+let blockSec = document.querySelectorAll('.block__inner');
+// todo add '.modal-active' to active modal block
+
+modalNoReward.addEventListener('click', (e) => {
+    let target = e.target.classList.value;
+    console.log(target)
+    if (target === 'modal__rad' || target === 'block__header') {
+        blockSec.forEach((e) => e.classList.remove('modal-active'))
+        modalNoReward.classList.add('modal-active')
+    }
+})
+modalBamboo.addEventListener('click', (e) => {
+    let target = e.target.classList.value;
+    console.log(target)
+    if (target === 'modal__rad' || target === 'block__header') {
+        blockSec.forEach((e) => e.classList.remove('modal-active'))
+        modalNoReward.classList.add('modal-active')
+    }
+})
