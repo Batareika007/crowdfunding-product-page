e2c4564f0ecaa78bb262ec4679e26c1e41d0b309
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-09-16 00:00:20 +0300

    popup modal  on click
    need to improve some style, need to use grid for mobile, restyle modal block

--- js/script.js
+++ js/script.js
@@ -1,39 +1,70 @@
-let hamburger = document.querySelector('.hamburger');
-let navList = document.querySelector('.nav-list');
-let bookmarkBtn = document.querySelector('.block__btn2');
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const bookmarkBtn = document.querySelector('.block__btn2');
 
-
+// hamburger menu close and open
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     navList.classList.toggle('is-active');
 })
 
+// bookmarked btn
 bookmarkBtn.addEventListener('click', () => {
     bookmarkBtn.classList.add('active-btn');
     document.querySelector('.button-text').innerHTML = "Bookmarked"
 })
 
-let modalNoReward = document.querySelector('.modal__no-rew');
-let modalBamboo = document.querySelector('.modal__bamboo');
-let modalBlack = document.querySelector('.modal__black');
-let modalMahogany = document.querySelector('.modal__mahogany');
+const modalNoReward = document.querySelector('.modal__no-rew');
+const modalBamboo = document.querySelector('.modal__bamboo');
+const modalBlack = document.querySelector('.modal__black');
+const modalMahogany = document.querySelector('.modal__mahogany');
+const blockSec = document.querySelectorAll('.block__inner');
 
-let blockSec = document.querySelectorAll('.block__inner');
-// todo add '.modal-active' to active modal block
 
+// close modal 
+document.querySelector('.modal__close img').addEventListener('click', () => {
+    hamburger.classList.remove('is-active');
+    navList.classList.remove('is-active');
+    document.querySelector('.modal').classList.remove('active');
+    document.querySelector('body').classList.remove('active');
+})
+
+// open empty modal
+document.getElementById('openModal').addEventListener('click', () => {
+    document.querySelector('.modal').classList.add('active');
+    document.querySelector('body').classList.add('active');
+
+})
+
+// modal free open form
 modalNoReward.addEventListener('click', (e) => {
     let target = e.target.classList.value;
     console.log(target)
-    if (target === 'modal__rad' || target === 'block__header') {
-        blockSec.forEach((e) => e.classList.remove('modal-active'))
-        modalNoReward.classList.add('modal-active')
+    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
+        blockSec.forEach((e) => e.classList.remove('modal-active'));
+        modalNoReward.classList.add('modal-active');
     }
 })
+
+// modal 25$ open form
 modalBamboo.addEventListener('click', (e) => {
     let target = e.target.classList.value;
     console.log(target)
-    if (target === 'modal__rad' || target === 'block__header') {
+    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
         blockSec.forEach((e) => e.classList.remove('modal-active'))
-        modalNoReward.classList.add('modal-active')
+        modalBamboo.classList.add('modal-active')
+    }
+
+
+})
+
+
+// modal 75$ open form
+modalBlack.addEventListener('click', (e) => {
+    let target = e.target.classList.value;
+    console.log(target)
+    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
+        blockSec.forEach((e) => e.classList.remove('modal-active'))
+        modalBlack.classList.add('modal-active')
     }
 })
