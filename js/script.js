e008bf2c909e5ba2aaf0a0d346c22be0979c69a1
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-09-17 23:42:03 +0300

    added popup window.
    JS on modal windows working.
    need to create count functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,27 @@ const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
 const bookmarkBtn = document.querySelector('.block__btn2');
 
+// modal vars
+const modalNoReward = document.querySelector('.modal__no-reward');
+const modalBamboo = document.querySelector('.modal__bamboo');
+const modalBlack = document.querySelector('.modal__black');
+const modalMahogany = document.querySelector('.modal__mahogany');
+const allModals = document.querySelectorAll('.modal__inner')
+
+// thanku 
+const thankuModal = document.getElementById('thanku');
+const thankuModalBtn = document.getElementById('thanku-btn');
+
+// remove blackBodyCover
+const removeBodyBlack = () => {
+    document.querySelector('body').classList.remove('active');
+}
+
+thankuModalBtn.addEventListener('click', () => {
+    removeBodyBlack()
+    thankuModal.classList.remove('active');
+})
+
 // hamburger menu close and open
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
@@ -14,57 +35,46 @@ bookmarkBtn.addEventListener('click', () => {
     document.querySelector('.button-text').innerHTML = "Bookmarked"
 })
 
-const modalNoReward = document.querySelector('.modal__no-rew');
-const modalBamboo = document.querySelector('.modal__bamboo');
-const modalBlack = document.querySelector('.modal__black');
-const modalMahogany = document.querySelector('.modal__mahogany');
-const blockSec = document.querySelectorAll('.block__inner');
-
-
 // close modal 
 document.querySelector('.modal__close img').addEventListener('click', () => {
     hamburger.classList.remove('is-active');
     navList.classList.remove('is-active');
     document.querySelector('.modal').classList.remove('active');
-    document.querySelector('body').classList.remove('active');
+    removeBodyBlack()
+    allModals.forEach(element => {
+        element.classList.remove('active')
+    });
 })
 
 // open empty modal
-document.getElementById('openModal').addEventListener('click', () => {
+const openModal = document.getElementById('openModal');
+
+openModal.addEventListener('click', () => {
     document.querySelector('.modal').classList.add('active');
     document.querySelector('body').classList.add('active');
-
-})
-
-// modal free open form
-modalNoReward.addEventListener('click', (e) => {
-    let target = e.target.classList.value;
-    console.log(target)
-    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
-        blockSec.forEach((e) => e.classList.remove('modal-active'));
-        modalNoReward.classList.add('modal-active');
-    }
-})
-
-// modal 25$ open form
-modalBamboo.addEventListener('click', (e) => {
-    let target = e.target.classList.value;
-    console.log(target)
-    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
-        blockSec.forEach((e) => e.classList.remove('modal-active'))
-        modalBamboo.classList.add('modal-active')
-    }
-
-
 })
 
 
-// modal 75$ open form
-modalBlack.addEventListener('click', (e) => {
-    let target = e.target.classList.value;
-    console.log(target)
-    if (target === 'modal__radio' || target === 'modal__header' || target === 'modal-rad' || target === 'block__price' || target === 'block__header') {
-        blockSec.forEach((e) => e.classList.remove('modal-active'))
-        modalBlack.classList.add('modal-active')
-    }
+// function to open form modal
+const openModalForm = (e) => {
+    document.querySelectorAll('.modal__inner').forEach(element => {
+        element.classList.remove('active')
+    });
+    e.classList.add('active')
+
+}
+
+allModals.forEach((e) => {
+    e.addEventListener('click', (e) => {
+        if (e.composedPath().includes(modalNoReward)) {
+            console.log('no reward')
+            openModalForm(modalNoReward)
+        } else if (e.composedPath().includes(modalBamboo)) {
+            console.log('25$ or more')
+            openModalForm(modalBamboo)
+        } else if (e.composedPath().includes(modalBlack)) {
+            console.log('75$ or more')
+            openModalForm(modalBlack)
+        }
+    })
 })
